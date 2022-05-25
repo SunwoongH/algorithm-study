89cc621c12813e0dde398bf141b9c9b77985fe15
Ahyeon Lim <80513699+ahyeon-github@users.noreply.github.com>
2022-05-25 18:09:19 +0900

    Delete 1주차 directory

--- 1주차/valid_palindrome_lah.py
+++
@@ -1,9 +0,0 @@
-#1. Palindrome Algorithm 유효한 팰린드롬 판별하기
-# 팰린드롬 : 거꾸로 읽어도 제대로 읽는 것
-
-def Valid_Palindrome(self, s:str) -> str:
-    for i in range(len(s)):
-        for j in range(i+1):
-            temp =  s[j: len(s) - i+j]
-            if temp == temp [: : -1]:
-                return temp
