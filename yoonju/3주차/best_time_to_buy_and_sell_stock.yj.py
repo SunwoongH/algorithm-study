0f491f38cff916262857328a52fd485167e794ba
yoonju <angelkim0409@naver.com>
2022-06-22 11:05:33 +0900

    yoonju: 3주차 풀이

---
+++ yoonju/3주차/best_time_to_buy_and_sell_stock.yj.py
@@ -0,0 +1,11 @@
+import sys
+
+def maxProfit(self, prices):
+  profit = 0
+  min_price = sys.maxsize
+
+  for price in prices:
+    min_price = min(min_price, price)
+    profit = max(profit, price - min_price)
+
+  return profit
---
+++ yoonju/3주차/two_sum_yj.py
@@ -0,0 +1,12 @@
+def twoSum(nums, target):
+  nums_map = {}
+        
+  for i, num in enumerate(nums):
+    if target - num in nums_map:
+      return [nums_map[target-num], i]
+    nums_map[num] = i
+
+nums = [2, 7, 11, 15]
+target  = 9
+
+print(twoSum(nums, target))
